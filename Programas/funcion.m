5a75ade2d08d528f627de7875d8464130c74f2b8
Semiramis de la Cruz <90874653+semilun4@users.noreply.github.com>
2022-05-20 12:58:43 -0500

    Update funcion.m

--- Programas/funcion.m
+++ Programas/funcion.m
@@ -1,12 +1,12 @@
 % Lenguaje: Matlab
 % Programa para evaluar una funcion
 % Nombre del archivo: funcion.m
-% García de la Cruz Semiramís
+% GarcÃ­a de la Cruz SemiramÃ­s
 % De la Torre Ortiz Bibiana
 % Bautista Lopez Sara
 % Nicolas Palacion Daniel Isai 
 %
-% Definición: [F] = funcion(x)
+% DefiniciÃ³n: [F] = funcion(x)
 % 
 % Entrada: 
 %   x = valor en el que se quiere evaluar
@@ -21,21 +21,6 @@ function [F] = funcion(x)
 % Funciones univariables
   % F = (100-x)^2;
  
-% Ejemplo profesora.
-  % F = 3*x(1)^2 + 3*x(1)*x(2) + 4*x(2)^2; 
-  % F = 4*x(1)+4*x(2)^3-10*x(2);
-
-% Funciones Tarea 4
-  % F = 8*x(1)^2 + 4*x(1)*x(2) +5*x(2)^2;
-  % F = 2*x(1)^3 +4*x(1)*x(2)^3 - 10*x(1)*x(2) + x(2)^2;
-  % F = 2*x(1)^2 - 1.05*x(1)^4 + (x(1)^6)/6 + x(1)*x(2) + x(2)^2;
-  % F = (x(1)+2*x(2) -7)^2 + (2*x(1) + x(2) -5)^2;
-  
-% Funciones Tarea 6
-  % F = 3*x(1)^2+2*x(2)^2+x(3)^2-2*x(1)*x(2)-2*x(1)*x(3)+2*x(2)*x(3)-6*x(1)-4*x(2)-2*x(3);
-  % F =((x(2)-((5.1/(4*pi^2))*x(1)^2)+((5*x(1))/pi)-6)^2+((10*(1-(1/(8*pi))))*cos(x(1)))+10);
-  % F = (x(2) - (5.1)/(4*pi^2)*x(1)^2+(5/pi)*x(1)-6)^2+10*(1-(1/(8*pi)))*cos(x(1))+10;
-
 % Funciones Newton
   % F = (x(1)+10*x(2))^2 + 5*(x(3)-x(4))^2+(x(2)-2*x(3))^4+10*(x(1)-x(4))^4;
   % F = x(1)^2 + 2*x(2)^2 + 3*x(3)^2 + 4*x(4)^2 + (x(1) +x(2) +x(3) +x(4))^2;
